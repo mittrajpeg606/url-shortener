b03d299d545f1cad073feaf14f91afc0ab1321bf
Priyaanshu Mittra <87920994+mittrajpeg606@users.noreply.github.com>
2026-06-28 20:49:47 +0530

    Update UrlConstants.java

--- backend/src/main/java/com/priyaanshu/url_shortener/constants/UrlConstants.java
+++ backend/src/main/java/com/priyaanshu/url_shortener/constants/UrlConstants.java
@@ -2,7 +2,7 @@ package com.priyaanshu.url_shortener.constants;
 
 public final class UrlConstants {
 
-    public static final String BASE_SHORT_URL ="http://localhost:8181/api/v1/urls/";
+    public static final String BASE_SHORT_URL ="https://url-shortener-w5f1.onrender.com/api/v1/urls/";
 
     private UrlConstants() {}
 }
